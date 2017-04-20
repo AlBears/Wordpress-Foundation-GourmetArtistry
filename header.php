f90fe61c38f381b1b60b04969a53d29faded7a18
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-04-20 19:49:33 +0300

    finish header

--- header.php
+++ header.php
@@ -30,7 +30,7 @@
 		<?php endif; // End header image check. ?>
 
 
-				<div class="site-branding">
+				<div class="site-branding text-center">
 					<?php
 					if ( is_front_page() && is_home() ) : ?>
 						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
