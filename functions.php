a66db53b3fd7a62067b1a9d17d60b61ec36c65cb
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-05-01 17:58:41 +0300

    adding widget

--- functions.php
+++ functions.php
@@ -162,3 +162,8 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/**
+ * Widgets.
+ */
+require get_template_directory() . '/inc/widgets.php';
